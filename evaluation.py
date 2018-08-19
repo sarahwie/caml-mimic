1eafba8af2ca81e0477965b4098e39778808ad91
Sarah Wiegreffe <wiegreffesa@g.cofc.edu>
2018-08-18 22:22:39 -0400

    updates for precision-recall AUC calculations

--- evaluation.py
+++ evaluation.py
@@ -9,7 +9,7 @@ import numpy as np
 import os
 import sys
 
-from sklearn.metrics import roc_curve, auc, precision_recall_curve
+from sklearn.metrics import roc_curve, auc, precision_recall_curve, average_precision_score
 from tqdm import tqdm
 
 from constants import *
@@ -171,16 +171,24 @@ def auc_metrics(yhat_raw, y, ymic):
         return
     fpr = {}
     tpr = {}
+    prec = {}
+    rec = {}
+    avg_precision = {}
     roc_auc = {}
     #get AUC for each label individually
     relevant_labels = []
     relevant_labels_pr = []
+    relevant_labels_avg_precision = []
     auc_labels = {}
     for i in range(y.shape[1]):
         #only if there are true positives for this label
         if y[:,i].sum() > 0:
             fpr[i], tpr[i], _ = roc_curve(y[:,i], yhat_raw[:,i])
             prec[i], rec[i], _ = precision_recall_curve(y[:,i], yhat_raw[:,i])
+            avg_precision[i] = average_precision_score(y[:,i], yhat_raw[:,i])
+            if not np.isnan(avg_precision[i]):
+                auc_labels["avg_precision_%d" % i] = avg_precision[i]
+                relevant_labels_avg_precision.append(i)
             if len(fpr[i]) > 1 and len(tpr[i]) > 1:
                 auc_score = auc(fpr[i], tpr[i])
                 if not np.isnan(auc_score): 
@@ -192,15 +200,20 @@ def auc_metrics(yhat_raw, y, ymic):
                     auc_labels["aucpr_%d" % i] = auc_pr
                     relevant_labels_pr.append(i)
 
+
     #macro-AUC: just average the auc scores
     aucs = []
     aucs_pr = []
+    avg_precs = []
     for i in relevant_labels:
         aucs.append(auc_labels['auc_%d' % i])
     for i in relevant_labels_pr:
         aucs_pr.append(auc_labels['aucpr_%d' % i])
+    for i in relevant_labels_avg_precision:
+        avg_precs.append(auc_labels["avg_precision_%d" % i])
     roc_auc['auc_macro'] = np.mean(aucs)
     roc_auc['aucpr_macro'] = np.mean(aucs_pr)
+    roc_auc['avg_prec_macro'] = np.mean(avg_precs)
 
     #micro-AUC: just look at each individual prediction
     yhatmic = yhat_raw.ravel()
@@ -208,6 +221,7 @@ def auc_metrics(yhat_raw, y, ymic):
     prec["micro"], rec["micro"], _ = precision_recall_curve(ymic, yhatmic)
     roc_auc["auc_micro"] = auc(fpr["micro"], tpr["micro"])
     roc_auc["aucpr_micro"] = auc(rec["micro"], prec["micro"])
+    roc_auc['avg_prec_micro'] = average_precision_score(ymic, yhatmic)
 
     return roc_auc
 
@@ -349,6 +363,10 @@ def print_metrics(metrics):
     for metric, val in metrics.items():
         if metric.find("rec_at") != -1:
             print("%s: %.4f" % (metric, val))
+    print("MACRO AUCPR, MICRO AUCPR")
+    print("%.4f, %.4f" % (metrics["aucpr_macro"], metrics["aucpr_micro"]))
+    print("AVERAGE PREC MACRO, MICRO")
+    print("%.4f, %.4f" % (metrics["avg_prec_macro"], metrics["avg_prec_micro"]))
     print()
 
 if __name__ == "__main__":
